985f2c737df47d26fc5ec7e2ffddbec4944bd106
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-11-06 19:53:19 -0700

    update project description in package.json

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var app = express();
+'use strict'
+
+var express = require('express')
+var app = express()
 
 //Create a static file server
 app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
+  app.use(express.static(__dirname + '/public'))
+})
 
 //Get the dummy data
-require('./server/ddata.js');
+require('./server/ddata.js')
 
-var port = 8080;
-app.listen(port);
-console.log('Express server started on port %s', port);
+var port = 8080
+app.listen(port)
+console.log('Express server started on port %s', port)
