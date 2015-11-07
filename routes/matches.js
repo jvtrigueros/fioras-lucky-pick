ea54d4c0e7b47489620a55f3416b43b71537098d
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-11-07 14:56:34 -0700

    remove semicolons and add new router for matches

--- routes/matches.js
+++ routes/matches.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  res.sent
+    res.render('matches', { title: "Fiora's Lucky Pick",welcome: "Hi Please review your matches:"});
+
 });
 
 module.exports = router;
