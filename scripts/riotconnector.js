8744d20759029e9a5f18361f47eaa8b2d8074269
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-11-08 14:16:39 -0700

    add api endpoint for league/by-summoner/:id/entry

--- scripts/riotconnector.js
+++ scripts/riotconnector.js
@@ -2,15 +2,12 @@
 
 var request = require('request')
 
-var API_KEY = ''
-var REGION = ''
+var summoner = { apiUrl: 'v1.4/summoner/' }
+  , league = { apiUrl: 'v2.5/league/' }
 
 function baseApiUrl(region) {
   return 'https://' + region + '.api.pvp.net/api/lol/' + region + '/'
 }
-
-var summoner = { apiUrl: 'v1.4/summoner/' }
-var stats = { apiUrl: 'v1.3/stats' }
 
 summoner.byName = function (region, key, summonerName, cb) {
   var url = baseApiUrl(region) + this.apiUrl + 'by-name/' + summonerName + '?api_key=' + key
@@ -19,10 +16,19 @@ summoner.byName = function (region, key, summonerName, cb) {
     if(!err) {
       if(res.statusCode == 404) {
         cb(new Error('Summoner with name ' + summonerName + ' does not exist.'), null)
-      } else
-        cb(null, JSON.parse(body)[summonerName])
-    } else
-      console.log(err, url)
+      } else cb(null, JSON.parse(body)[summonerName])
+    } else cb(err, null)
+  })
+}
+
+league.bySummonerEntry = function (region, key, summonerId, cb) {
+  var url = baseApiUrl(region) + this.apiUrl + 'by-summoner/' + summonerId + '/entry?api_key=' + key
+
+  request(url, function (err, res, body) {
+    if(!err && res.statusCode == 200) {
+      var result = JSON.parse(body)
+      cb(null, result[summonerId.toString()])
+    } else cb(err, null)
   })
 }
 
