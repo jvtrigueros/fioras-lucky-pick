c6f25d6216c45537eeeae349afb9a12119add65f
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-11-08 15:34:20 -0700

    delete default values that were used for testing

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,11 @@
-/**
- * Created by Daniel on 11/7/2015.
- */
-var express = require('express');
-var router = express.Router();
+'use strict'
+
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('profile', { });
-});
+    res.render('profile', { })
+})
 
-module.exports = router;
+module.exports = router
