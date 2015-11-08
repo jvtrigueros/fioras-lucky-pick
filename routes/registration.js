2e2d3d10716519791dafdc19e6ae277b4f50285a
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-11-08 15:29:58 -0700

    handle user registration

--- routes/registration.js
+++ routes/registration.js
@@ -1,42 +1,74 @@
 var express = require('express')
   , mongodb = require('mongodb')
   , nconf = require('nconf')
+  , riotConnector = require('../scripts/riotconnector')
   , router = express.Router()
 
-var regions = [ "BR"
-              , "EUNE"
-              , "EUW"
-              , "KR"
-              , "LAN"
-              , "LAS"
-              , "NA"
-              , "OCE"
-              , "RU"
-              , "TR"
+var regions = [ 'br'
+              , 'eune'
+              , 'euw'
+              , 'kr'
+              , 'lan'
+              , 'las'
+              , 'na'
+              , 'oce'
+              , 'ru'
+              , 'tr'
               ]
 
-var roles = [ "TOP"
-            , "MID"
-            , "JUNGLE"
-            , "ADC"
-            , "SUPPORT"
+var roles = [ 'top'
+            , 'mid'
+            , 'jungle'
+            , 'adc'
+            , 'support'
             ]
 
 var MongoClient = mongodb.MongoClient
   , mongoUrl = 'mongodb://' + nconf.get('db:user') + ':' + nconf.get('db:pass') + '@' + nconf.get('db:url')
+  , riot = riotConnector(nconf.get('riot:key'))
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   var user = req.user || {id: null}
 
-  res.render('registration', { regions: regions, roles: roles, _id: user.id })
+  res.render('registration', {
+    regions: regions, roles: roles, _id: user.id, helpers: {
+      toUpperCase: function (str) {
+        return str.toUpperCase(str)
+      }
+    }
+  })
 })
 
 router.post('/', function (req, res) {
-  MongoClient.connect(mongoUrl, function (err, db) {
-    // TODO: talk to rito to get their rank
-    db.collection('summoners').insertOne(req.body, function (err, result) {
-      res.redirect('/profile')
-    })
+  var result = req.body
+
+  riot.summoner.byName(result.region, result.summoner, function (err, summonerDto) {
+    if (!err) {
+      result.summonerId = summonerDto.id
+      console.log('registration.post.byName', result)
+      riot.league.bySummonerEntry(result.region, result.summonerId, function (err, leagues) {
+        if (!err)
+          leagues
+            .filter(function (league) {
+              return league.queue == 'RANKED_SOLO_5x5'
+            })
+            .forEach(function (league) {
+              result.tier = league.tier.toLowerCase()
+              result.division = league.entries[0].division
+
+              MongoClient.connect(mongoUrl, function (err, db) {
+                db.collection('summoners').insertOne(result, function (err) {
+                  if(!err)
+                    res.redirect('/profile')
+                  else
+                    res.redirect('back')
+                })
+              })
+            })
+        else
+          res.redirect('back')
+      })
+    } else res.redirect('back')
   })
 })
 
