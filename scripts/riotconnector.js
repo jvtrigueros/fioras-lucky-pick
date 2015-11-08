b6ebc39ad888d110a044e484e8fcadcf1ba1906f
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-11-08 14:18:58 -0700

    fix formatting

--- scripts/riotconnector.js
+++ scripts/riotconnector.js
@@ -2,8 +2,8 @@
 
 var request = require('request')
 
-var summoner = { apiUrl: 'v1.4/summoner/' }
-  , league = { apiUrl: 'v2.5/league/' }
+var summoner = {apiUrl: 'v1.4/summoner/'}
+  , league = {apiUrl: 'v2.5/league/'}
 
 function baseApiUrl(region) {
   return 'https://' + region + '.api.pvp.net/api/lol/' + region + '/'
@@ -13,8 +13,8 @@ summoner.byName = function (region, key, summonerName, cb) {
   var url = baseApiUrl(region) + this.apiUrl + 'by-name/' + summonerName + '?api_key=' + key
 
   request(url, function (err, res, body) {
-    if(!err) {
-      if(res.statusCode == 404) {
+    if (!err) {
+      if (res.statusCode == 404) {
         cb(new Error('Summoner with name ' + summonerName + ' does not exist.'), null)
       } else cb(null, JSON.parse(body)[summonerName])
     } else cb(err, null)
@@ -25,7 +25,7 @@ league.bySummonerEntry = function (region, key, summonerId, cb) {
   var url = baseApiUrl(region) + this.apiUrl + 'by-summoner/' + summonerId + '/entry?api_key=' + key
 
   request(url, function (err, res, body) {
-    if(!err && res.statusCode == 200) {
+    if (!err && res.statusCode == 200) {
       var result = JSON.parse(body)
       cb(null, result[summonerId.toString()])
     } else cb(err, null)
@@ -35,7 +35,9 @@ league.bySummonerEntry = function (region, key, summonerId, cb) {
 module.exports = function (region, key) {
   return {
     summoner: {
-      byName: function (summonerName, cb) { summoner.byName(region, key, summonerName, cb) }
+      byName: function (summonerName, cb) {
+        summoner.byName(region, key, summonerName, cb)
+      }
     }
   }
 }
