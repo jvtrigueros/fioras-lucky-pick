edc6e97f14df0aa9666e2944f743900e056d97c5
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-11-08 13:13:44 -0700

    implement an adapter that talks to the Riot API

--- scripts/riotconnector.js
+++ scripts/riotconnector.js
@@ -1,3 +1,37 @@
-/**
- * Created by Daniel on 11/7/2015.
- */
+'use strict'
+
+var request = require('request')
+
+var API_KEY = ''
+var REGION = ''
+var API_VERSION = 'v1.4'
+
+function baseApiUrl(region) {
+  return 'https://' + region + '.api.pvp.net/api/lol/' + region + '/' + API_VERSION + '/'
+}
+
+var summoner = {}
+
+summoner.byName = function (region, key, summonerName, cb) {
+  var url = baseApiUrl(region) + 'summoner/by-name/' + summonerName + '?api_key=' + key
+  request(url, function (err, res, body) {
+    if(!err) {
+      if(res.statusCode == 404) {
+        var summonerDto = {}
+        summonerDto[summonerName] = {}
+        cb(summonerDto)
+      } else
+        cb(JSON.parse(body)[summonerName])
+    } else
+      console.log(err, url)
+  })
+}
+
+module.exports = function (region, key) {
+  return {
+    summoner: {
+      byName: function (summonerName, cb) { summoner.byName(region, key, summonerName, cb) }
+    }
+  }
+}
+
