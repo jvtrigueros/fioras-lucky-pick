22705b1c35e9cb17ab0f308ce5a9f770540cbf8d
Jose V. Trigueros <j.v.trigueros@gmail.com>
2015-11-08 17:41:42 -0700

    show summoner icon in the matches page

--- routes/matches.js
+++ routes/matches.js
@@ -7,12 +7,13 @@ var express = require('express')
 
 var MongoClient = mongodb.MongoClient
   , mongoUrl = 'mongodb://' + nconf.get('db:user') + ':' + nconf.get('db:pass') + '@' + nconf.get('db:url')
+  , iconUrl = 'http://ddragon.leagueoflegends.com/cdn/5.22.1/img/profileicon/'
 
 router.get('/', function (req, res, next) {
   MongoClient.connect(mongoUrl, function (err, db) {
-    db.collection('summoners').find().limit(10).toArray(function (err, summoners) {
+    db.collection('summoners').find().limit(9).toArray(function (err, summoners) {
       if(!err)
-        res.render('matches', {summoners: summoners})
+        res.render('matches', {summoners: summoners, iconUrl: iconUrl})
       else
         res.render('matches')
     })
